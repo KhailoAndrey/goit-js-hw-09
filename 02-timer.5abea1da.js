d11ed03b833eb6253604f4fabcf2590245a50416
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2023-01-27 16:09:16 +0000

    Deploying to gh-pages from @ KhailoAndrey/goit-js-hw-09@10cb2fc813a2aa236b04db2ca0eb797c43e56d5b 🚀

--- 02-timer.5abea1da.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,n;o="Mango",n=function(o){console.log(o)},console.log("Fetching data for ".concat(o)),setTimeout((function(){n("success value")}),2e3)}();
-//# sourceMappingURL=02-timer.5abea1da.js.map
