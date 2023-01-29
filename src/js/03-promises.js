0b346090ee94ad995c3b3faf843f0dad1d559a58
KhailoAndrey <hap512@gmail.com>
2023-01-29 15:22:59 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,25 +1,22 @@
 import Notiflix from 'notiflix';
 import Throttle from 'Lodash';
 
-const refs = {
-  submitBtn: document.querySelector('button'),
-  form: document.querySelector('.form'),
-};
+const form = document.querySelector('.form');
 
-let amount = 0;
-let delay = 0;
-let step = 0;
+// let position = 0;
+// let delay = 0;
+// let step = 0;
 
-function getFormValue() {
-    amount = refs.form.elements.amount.value;
-    delay = refs.form.elements.delay.value;
-    step = refs.form.elements.step.value; 
-  console.log('amount', amount);
-  console.log('delay', delay);
-  console.log('step', step);
-}
-refs.form.addEventListener('input', getFormValue);
-refs.submitBtn.addEventListener('submit', startPromises);
+// function getFormValue() {
+//     amount = refs.form.elements.amount.value;
+//     delay = refs.form.elements.delay.value;
+//     step = refs.form.elements.step.value;
+//   console.log('amount', amount);
+//   console.log('delay', delay);
+//   console.log('step', step);
+// }
+// refs.form.addEventListener('input', getFormValue);
+form.addEventListener('submit', startPromises);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -30,9 +27,8 @@ function createPromise(position, delay) {
     } else {
       // Reject
       reject({ position, delay });
-    }    
-  }
-  )
+    }
+  });
 }
 //  position = 2;
 //  delay = 1000;
@@ -45,34 +41,35 @@ function createPromise(position, delay) {
 //     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 //   });
 
-function startPromises() { 
-  // position = refs.form.elements.amount.value;
-  // delay = refs.form.elements.delay.value;
-  // step = refs.form.elements.step.value;
-  position = 2;
-  delay = 1000;
-  step = 1500;
-  
-  console.log(position, delay, step);
-  // setTimeout(({ position, delay }) => {
-  //   for (const i = 1; i <= position; i++) {
-  //     setTimeout(({ position, delay }) => {
-//         createPromise(position, delay)
-//           .then(({ position, delay }) => {
-//             Notiflix.Notify.info(
-//               `✅ Fulfilled promise ${position} in ${delay}ms`
-//             );
-//           })
-//           .catch(({ position, delay }) => {
-//             Notiflix.Notify.failure(
-//               `❌ Rejected promise ${position} in ${delay}ms`
-//             );
-//           });
-// //       }, step);
-//     }
-//   }, delay);
-
-
+function startPromises(event) {
+  event.preventDefault();
+  // console.log(event.currentTarget.elements.amount.value);
+  const amount = event.currentTarget.elements.amount.value;
+  const delay = event.currentTarget.elements.delay.value;
+  const step = event.currentTarget.elements.step.value;
+  let position = 0;
+  // let currentDelay = delay;
+  console.log(delay, step, amount);
+  setTimeout(() => {
+    for (let i = 1; i < amount; i++) {
+      position = i;
+      console.log(position);
+      // setInterval(() => {
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.info(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+      // }, step);
+      // currentDelay = currentDelay + step;
+    }
+  }, delay);
 }
 
 // createPromise(2, 1500)
@@ -82,4 +79,3 @@ function startPromises() {
 //   .catch(({ position, delay }) => {
 //     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 //   });
-
