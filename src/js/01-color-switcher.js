95b1cf71b019fafed180e9edf8c3a445ff8af9be
KhailoAndrey <hap512@gmail.com>
2023-01-30 13:28:50 +0200

    reclaim task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,9 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const bodyBackgroundColor = document.querySelector('body');
 let timerId = null;
-
+stopBtn.disabled = true;
 startBtn.addEventListener('click', () => {
+  stopBtn.disabled = false;
   startBtn.disabled = true;
   timerId = setInterval(() => {
     bodyBackgroundColor.style.backgroundColor = getRandomHexColor();
@@ -13,6 +14,7 @@ startBtn.addEventListener('click', () => {
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
   startBtn.disabled = false;
+  stopBtn.disabled = true;
 });
 
 function getRandomHexColor() {
