ffd897062f04ec70665a708e2763260732e826db
KhailoAndrey <hap512@gmail.com>
2023-01-30 21:34:39 +0200

    reclaim task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,40 +4,42 @@ const form = document.querySelector('.form');
 const startBtn = document.querySelector('button');
 form.addEventListener('submit', startPromises);
 
-function createPromise(position, delay) {
+function createPromise({ position, currentDelay, amount, step }) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
+      if (position === Number(amount.value)) {
+        startBtn.disabled = false;
       }
-    }, delay);
+      if (shouldResolve) {
+        resolve({ position, currentDelay });
+      } else {
+        reject({ position, currentDelay });
+      }
+    }, currentDelay);
   });
 }
 
 function startPromises(event) {
   event.preventDefault();
-  form.disabled = true;
   const { amount, delay, step } = event.currentTarget.elements;
   let position = 0;
   let currentDelay = Number(delay.value);
+  startBtn.setAttribute('disabled', true);
   for (let i = 1; i <= amount.value; i++) {
-    // if ((i = amount.value)) {
-    // }
     position = i;
     console.log('position', position, 'step', currentDelay);
-    createPromise(position, currentDelay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    createPromise({ position, currentDelay, amount, step })
+      .then(({ position, currentDelay }) => {
+        Notiflix.Notify.info(
+          `✅ Fulfilled promise ${position} in ${currentDelay}ms`
+        );
       })
-      .catch(({ position, delay }) => {
+      .catch(({ position, currentDelay }) => {
         Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
+          `❌ Rejected promise ${position} in ${currentDelay}ms`
         );
       });
     currentDelay = currentDelay + Number(step.value);
   }
-  form.disabled = false;
 }
